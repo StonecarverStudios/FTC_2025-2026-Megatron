d96f34faea57a8c1ebcac797a73e34791b01da35
Ethan Schiappapietra <ethansch26@gmail.com>
2026-01-27 22:21:17 -0800

    Remove commented-out launching system code
    Removed commented-out code related to the launching system.

---
+++ LegacyMegatron.java
@@ -0,0 +1,51 @@
+
+            //Launching and trigger system Manual (Legacy)
+                // //Launching
+                // if (gamepad2.right_trigger > 0) {
+                    
+                //     double targetVelocity;
+                    
+                //     if(farRange){
+                //         targetVelocity = farVelocity;
+                //     }else{
+                //         targetVelocity = closeVelocity;
+                //     }
+                    
+    
+                //     leftThrow.setVelocity(targetVelocity);
+                //     rightThrow.setVelocity(targetVelocity);
+                // } else {
+                //     leftThrow.setVelocity(0);
+                //     rightThrow.setVelocity(0);
+                // }
+                            
+                // //Trigger
+                // if(gamepad2.a){
+                //     trigger.setPosition(0.2);
+                //     runTime.reset();
+                // }else if(runTime.seconds() > 0.5 ){
+                //     trigger.setPosition(0.6);
+                // }
+            
+             //Feeder Logic Legacy
+                // //Top Feeder
+                // if (gamepad2.right_stick_y != 0){
+                //     double feedPower = gamepad2.right_stick_y;
+                
+                //     topFeeder.setPower(feedPower);
+                // }else {
+                //     double feedPower = 0;
+                
+                //     topFeeder.setPower(feedPower);
+                // }
+                
+                // //Bottom Feeder
+                // if (gamepad2.left_stick_y != 0){
+                //     double feedPower = -gamepad2.left_stick_y;
+                
+                //     bottomFeeder.setPower(feedPower);
+                // }else {
+                //     double feedPower = 0;
+                
+                //     bottomFeeder.setPower(feedPower);
+                // }
